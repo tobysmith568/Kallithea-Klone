5260be3b19910c2144e5354391aac2c7f11b3337
Toby Smith <tobysmith568@hotmail.co.uk>
2019-01-27 22:01:31 +0000

    Revert "Moved passwordChanged event to XAML"
    This reverts commit 82e3bbd170a5689ca4e17c3f1ef4e084460fdc04.

--- Kallithea Klone/Settings.xaml.cs
+++ Kallithea Klone/Settings.xaml.cs
@@ -1,4 +1,4 @@
-using Kallithea_Klone.Other_Classes;
+﻿using Kallithea_Klone.Other_Classes;
 using Newtonsoft.Json;
 using RestSharp;
 using System;
@@ -29,10 +29,13 @@ namespace Kallithea_Klone
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            TbxHost.Text = AccountSettings.Host;
             TbxAPIKey.Text = AccountSettings.APIKey;
+            TbxHost.Text = AccountSettings.Host;
             PbOne.Password = AccountSettings.Password;
             PbTwo.Password = AccountSettings.Password;
+
+            PbOne.PasswordChanged += PasswordChanged;
+            PbTwo.PasswordChanged += PasswordChanged;
 
             CbUpdates.IsChecked = AccountSettings.Updates;
         }
