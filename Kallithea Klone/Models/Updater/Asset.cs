d5c190b5acd1dad70b28213e6cd2c6fc4eb71444
Toby Smith <tobysmith568@hotmail.co.uk>
2019-06-05 22:10:37 +0100

    Added an updater interface

---
+++ Kallithea Klone/Models/Updater/Asset.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+
+namespace KallitheaKlone.Models.Updater
+{
+    public class Asset
+    {
+        //  Variables
+        //  =========
+
+        private static readonly string[] dataSizes = { "B", "KB", "MB", "GB" };
+        private int bytes;
+
+        //  JSON Properties
+        //  ===============
+
+        [JsonProperty("browser_download_url")]
+        public string URL { get; set; }
+
+        [JsonProperty("size")]
+        public int Bytes
+        {
+            get => bytes;
+            set
+            {
+                bytes = value;
+
+                int len = value;
+                int order = 0;
+                while (len >= 1024 && order < dataSizes.Length - 1)
+                {
+                    order++;
+                    len /= 1024;
+                }
+                ReadableSize = string.Format("{0:0.##} {1}", len, dataSizes[order]);
+            }
+        }
+
+        //  Properties
+        //  ==========
+
+        public string ReadableSize { get; private set; }
+
+        public string UpdateURL { get; set; }
+    }
+}
---
+++ Kallithea Klone/Models/Updater/GithubRelease.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+namespace KallitheaKlone.Models.Updater
+{
+    public class GithubRelease
+    {
+        private Asset[] assets;
+
+        //  JSON Properties
+        //  ===============
+
+        [JsonProperty("html_url")]
+        public string URL { get; set; }
+
+        [JsonProperty("tag_name")]
+        public string Tag { get; set; }
+
+        [JsonProperty("draft")]
+        public bool IsDraft { get; set; }
+
+        [JsonProperty("assets")]
+        public Asset[] Assets
+        {
+            get => assets;
+            set
+            {
+                assets = value;
+
+                foreach (Asset asset in Assets)
+                {
+                    asset.UpdateURL = URL;
+                }
+            }
+        }
+    }
+}
