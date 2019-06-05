c277ff9ce516229f1f7e2409df87e13ddb7b3ca8
Toby Smith <tobysmith568@hotmail.co.uk>
2019-06-05 22:45:19 +0100

    Added a settings interface
    Added a settings interface

---
+++ Kallithea Klone/Models/Settings/ISettings.cs
@@ -0,0 +1,16 @@
+﻿namespace KallitheaKlone.Models.Settings
+{
+    public interface ISettings
+    {
+        //  Properties
+        //  ==========
+
+        bool JustInstalled { get; set; }
+
+        //  Methods
+        //  =======
+
+        void Upgrade();
+        void SaveAll();
+    }
+}
