fdb9d38333ee9eca44e5497ddaacfac8f908e088
Toby Smith <tobysmith568@hotmail.co.uk>
2019-06-11 00:47:22 +0100

    Set newtonsoft converter to use full enum names

--- WPF Application/Models/JSONConverter/NewtonSoftJSONConverter.cs
+++ WPF Application/Models/JSONConverter/NewtonSoftJSONConverter.cs
@@ -5,17 +5,28 @@ namespace KallitheaKlone.WPF.Models.JSONConverter
 {
     public class NewtonSoftJSONConverter : IJSONConverter
     {
+        private static readonly JsonSerializerSettings settings;
+
+        //  Constructors
+        //  ============
+
+        static NewtonSoftJSONConverter()
+        {
+            settings = new JsonSerializerSettings();
+            settings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
+        }
+
         //  Methods
         //  =======
 
         public T FromJson<T>(string json)
         {
-            return JsonConvert.DeserializeObject<T>(json);
+            return JsonConvert.DeserializeObject<T>(json, settings);
         }
 
         public string ToJson<T>(T model)
         {
-            return JsonConvert.SerializeObject(model);
+            return JsonConvert.SerializeObject(model, settings);
         }
     }
 }
