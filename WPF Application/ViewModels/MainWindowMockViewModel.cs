5b783562c2afaf980da74c5c5f21d60f38324b72
Toby Smith <tobysmith568@hotmail.co.uk>
2019-07-11 00:01:15 +0100

    Added a mock viewModel
    Added a mock viewModel

---
+++ WPF Application/ViewModels/MainWindowMockViewModel.cs
@@ -0,0 +1,23 @@
+﻿using KallitheaKlone.Models.Repositories;
+using KallitheaKlone.ViewModels;
+using KallitheaKlone.WPF.Models.Repositoties.Mercurial;
+using System.Collections.ObjectModel;
+
+namespace KallitheaKlone.WPF.ViewModels
+{
+    public class MainWindowMockViewModel : MainWindowViewModel
+    {
+        //  Constructors
+        //  ============
+
+        public MainWindowMockViewModel()
+        {
+            Repositories = new ObservableCollection<IRepository>()
+            {
+                new Repository(@"D:\Users\Toby\Downloads\V21product", "V22 Product")
+                {
+                }
+            };
+        }
+    }
+}
